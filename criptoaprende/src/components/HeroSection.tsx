716f0f87721342328488e5c35501a47f152df083
voidMuyTofu <fernandoperezdelatorremunoz@gmail.com>
2022-05-15 04:11:54 +0200

    Añadido parrafo explicativo y cambiado el estilo.

--- criptoaprende/src/components/HeroSection.tsx
+++ criptoaprende/src/components/HeroSection.tsx
@@ -1,9 +1,20 @@
 import styles from "../styles/HeroSection.module.css";
+import image from "../public/hero.svg";
 export default function HeroSection() {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.titulo}>Aprende </h1>
-            <img src="../public/image_hero.svg" />
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.titulo}>
+        Iniciate en el mundo de las{" "}
+        <span className={styles.subrayado}>criptos</span>
+      </h1>
+      <p className={styles.description}>
+        Aprende los primeros pasos para invertir en criptomonedas. Aquí te
+        enseñamos a crear y validar una cuenta en un exchange, a como invertir
+        en tu primera cripto, o incluso a ganar intereses por las criptomondeas
+        que ya tienes. Echa un vistazo a los tutoriales más populares justo
+        debajo.
+      </p>
+      <img src={image} className={styles.heroImage} />
+    </div>
+  );
 }
