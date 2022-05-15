87cedffa2b432a5abef73155a417a4586b9f4f37
voidMuyTofu <fernandoperezdelatorremunoz@gmail.com>
2022-05-15 04:12:37 +0200

    Cambiado el icono de la navbar cerrada y cambios en el estilo

--- criptoaprende/src/components/NavBar.tsx
+++ criptoaprende/src/components/NavBar.tsx
@@ -39,14 +39,20 @@ export default function NavBar() {
           </li>
         </ul>
       </nav>
-      <svg className={styles.svgMenu} viewBox="0 0 20 20" onClick={onClickMenu}>
+      <svg
+        className={styles.svgMenu}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+        onClick={onClickMenu}
+      >
         <path
-          fill="none"
-          d="M3.314,4.8h13.372c0.41,0,0.743-0.333,0.743-0.743c0-0.41-0.333-0.743-0.743-0.743H3.314
-								c-0.41,0-0.743,0.333-0.743,0.743C2.571,4.467,2.904,4.8,3.314,4.8z M16.686,15.2H3.314c-0.41,0-0.743,0.333-0.743,0.743
-								s0.333,0.743,0.743,0.743h13.372c0.41,0,0.743-0.333,0.743-0.743S17.096,15.2,16.686,15.2z M16.686,9.257H3.314
-								c-0.41,0-0.743,0.333-0.743,0.743s0.333,0.743,0.743,0.743h13.372c0.41,0,0.743-0.333,0.743-0.743S17.096,9.257,16.686,9.257z"
-        ></path>
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M4 6h16M4 12h16m-7 6h7"
+        />
       </svg>
     </header>
   );
