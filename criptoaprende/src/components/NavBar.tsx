55fed3be87d122560c8deb5adad8b6502206897b
voidMuyTofu <fernandoperezdelatorremunoz@gmail.com>
2022-05-25 11:04:10 +0200

    cambios

--- criptoaprende/src/components/NavBar.tsx
+++ criptoaprende/src/components/NavBar.tsx
@@ -2,33 +2,32 @@ import styles from "../styles/NavBar.module.css";
 import { useRef, useState } from "react";
 
 export default function NavBar() {
-    const navRef = useRef(null);
-    const [scrolled, setScrolled] = useState(false);
-    const [isClosed, setIsClosed] = useState(true);
-    const handleScroll = () => {
-        const offset = window.scrollY;
-        if (offset > 200) {
-            setScrolled(true);
-        } else {
-            setScrolled(false);
-        }
-    };
+  const navRef = useRef(null);
+  const [scrolled, setScrolled] = useState(false);
+  const [isClosed, setIsClosed] = useState(true);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
 
-    const onClickMenu = (event: any) => {
-        setIsClosed((prevCheck) => !prevCheck);
-    };
+  const onClickMenu = (event: any) => {
+    setIsClosed((prevCheck) => !prevCheck);
+  };
 
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.content}>
-                <h1>CryptoAprende</h1>
-                <ul className={styles.menuList}>
-                    <li>Tutoriales</li>
-                    <li>Tutoriales</li>
-                    <li>Tutoriales</li>
-                    <li>Tutoriales</li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.content}>
+        <h1 className={styles.titulo}>CryptoAprende</h1>
+        <ul className={styles.menuList}>
+          <li>Tutoriales</li>
+          <li>Tutoriales</li>
+          <li>Tutoriales</li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
