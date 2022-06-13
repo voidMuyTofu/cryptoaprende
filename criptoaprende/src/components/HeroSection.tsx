6e01b2d58c20b822e76c5ff7a0de8f100bf66f9e
Fernando <fernandoperezdelatorremunoz@gmail.com>
2022-06-13 16:36:46 +0200

    Añadida animacion de subrayado al titulo

--- criptoaprende/src/components/HeroSection.tsx
+++ criptoaprende/src/components/HeroSection.tsx
@@ -2,27 +2,27 @@ import styles from "../styles/HeroSection.module.css";
 import image from "../public/hero3.svg";
 import { useNavigate } from "react-router-dom";
 export default function HeroSection() {
-  const navigate = useNavigate();
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.titulo}>
-        Iniciate en el mundo de las{" "}
-        <span className={styles.subrayado}>criptos</span>
-      </h1>
-      <p className={styles.descripcion}>
-        Aprende los primeros pasos para invertir en criptomonedas. Crea una
-        cuenta, invierte en tu primera cripto, e incluso gana intereses por tus
-        criptomonedas.
-      </p>
-      <button
-        className={styles.botonTutoriales}
-        onClick={() => {
-          navigate("/tutoriales");
-        }}
-      >
-        TUTORIALES
-      </button>
-      <img src={image} className={styles.heroImage} />
-    </div>
-  );
+    const navigate = useNavigate();
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.titulo}>
+                Iniciate en el mundo de las{" "}
+                <div className={styles.subrayado}>criptos</div>
+            </h1>
+            <p className={styles.descripcion}>
+                Aprende los primeros pasos para invertir en criptomonedas. Crea
+                una cuenta, invierte en tu primera cripto, e incluso gana
+                intereses por tus criptomonedas.
+            </p>
+            <button
+                className={styles.botonTutoriales}
+                onClick={() => {
+                    navigate("/tutoriales");
+                }}
+            >
+                TUTORIALES
+            </button>
+            <img src={image} className={styles.heroImage} />
+        </div>
+    );
 }
