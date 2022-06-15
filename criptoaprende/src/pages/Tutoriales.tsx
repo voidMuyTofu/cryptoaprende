048b0ba459863195f4921b0d6c33f5a78e0797d7
voidMuyTofu <fernandoperezdelatorremunoz@gmail.com>
2022-06-16 01:01:17 +0200

    Cambios en la seccion de tutoriales

--- criptoaprende/src/pages/Tutoriales.tsx
+++ criptoaprende/src/pages/Tutoriales.tsx
@@ -2,10 +2,14 @@ import NavBar from "../components/NavBar";
 import styles from "../styles/Tutoriales.module.css";
 
 export default function Tutoriales() {
-    return (
-        <div className={styles.container}>
-            <NavBar />
-            <h1 className={styles.titulo}>Tutoriales</h1>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <NavBar />
+      <h1 className={styles.titulo}>Tutoriales</h1>
+      <p>
+        Elige el <div className={styles.subrayado}>nivel</div> que consideres
+        que más se ajusta al tuyo
+      </p>
+    </div>
+  );
 }
