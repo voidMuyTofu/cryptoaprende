32038ad676f6b5e8e9d2853f452b7dcc66162105
Fernando <fernandoperezdelatorremunoz@gmail.com>
2022-06-22 14:02:36 +0200

    Empezando a hacer componente para cards

---
+++ criptoaprende/src/data/LoMasPopular.ts
@@ -0,0 +1,19 @@
+const data = [
+  {
+    titulo: "Crea una cuenta en Coinbase",
+    descripcion: "Crea y verifica una cuenta en Coinbase",
+    tiempo: "5",
+  },
+  {
+    titulo: "Invierte usando Coinbase",
+    descripcion: "Realiza una inversión en una criptomoneda en Coinbase",
+    tiempo: "10",
+  },
+  {
+    titulo: "Gana intereses en Youholder",
+    descripcion: "Gana intereses semanalmente con tus criptomonedas",
+    tiempo: "15",
+  },
+];
+
+export default data;
