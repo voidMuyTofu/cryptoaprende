39d2100d3a9963fbd145765a4552eb846b2d2cf3
voidMuyTofu <fernandoperezdelatorremunoz@gmail.com>
2022-07-23 01:02:04 +0200

    Estructura de datos terminada con datos de prueba

--- criptoaprende/src/components/TutorialPage.tsx
+++ criptoaprende/src/components/TutorialPage.tsx
@@ -1,10 +1,71 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 
 export default function TutorialPage() {
+  interface ITutorial {
+    id: number;
+    url: string;
+    urlVideo: string;
+    titulo: string;
+    descripcion: string;
+    tiempo: number;
+    texto: string;
+  }
+
   const params = useParams();
+  const location = useLocation();
+  const nivel = location.pathname.split("/", 3)[2];
+  const urlFetch =
+    nivel == "principiante"
+      ? import.meta.env.VITE_API_PRINCIPIANTE
+      : nivel == "intermedio"
+      ? import.meta.env.VITE_API_INTERMEDIO
+      : nivel == "avanzado"
+      ? ""
+      : import.meta.env.VITE_API_AVANZADO;
+  const [data, setData] = useState<ITutorial[]>();
+
   useEffect(() => {
-    //Hacer fetch aqui con el parametro de la url params.idtutorial
+    fetch(urlFetch)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error HTTP: El status es ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        setData(actualData);
+      })
+      .catch((err) => {
+        setData([]);
+        console.log(err.message);
+      });
   }, []);
-  return <h1>sdfsdfdsdsf</h1>;
+
+  const filtrarPorId = (data: any) => {
+    if (data.url == params.idtutorial) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const tutoriales = data?.filter(filtrarPorId);
+  const tutorial = tutoriales != null ? tutoriales[0] : null;
+
+  return (
+    <>
+      <h1>dsadad</h1>
+      <h1>{tutorial?.url}</h1>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/jxT2HT7tF6g"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </>
+  );
 }
